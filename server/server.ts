e612ccc28510018d5105ad4f50b54377abdb2186
amitsaini-9 <amitsaini9086@gmail.com>
2025-01-29 14:50:26 +0530

    first commit

--- server/server.ts
+++ server/server.ts
@@ -2,20 +2,26 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import authRoutes from './routes/auth';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Connect to MongoDB
-mongoose.connect('mongodb+srv://root:root@requin-assesment.8cj4o.mongodb.net/?retryWrites=true&w=majority&appName=requin-assesment')
+const MONGODB_URI = process.env.MONGODB_URI as string;
+// Connect to MongoDB using the URI from the environment variables
+mongoose.connect(MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB', err));
 
 // Use authentication routes
 app.use('/auth', authRoutes);
 
-// Start the server
-app.listen(3001, () => {
-  console.log('Server running on port 3001');
+// Start the server on the port specified in the environment variables
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
